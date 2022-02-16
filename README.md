b20fb02a279059363c56616c2d7eb75e020a7201
AkberIqbal <akber.iqbal@gmail.com>
2022-02-16 15:49:11 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-9djpe5
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-9djpe5)
---
+++ index.ts
@@ -0,0 +1,63 @@
+// Import stylesheets
+import './style.css';
+
+// Write TypeScript code!
+const appDiv: HTMLElement = document.getElementById('app');
+appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+
+const f_Three_handleSubmit = async <T extends string>(
+  existingPromise: Promise<any>,
+  otherProp: string
+) => {
+  await existingPromise
+    .then((data) => {
+      console.log('3 handle submit running now', otherProp);
+    })
+    .catch((exp) => {
+      console.log('catch:', exp);
+    });
+};
+
+const f_One_Register = (someProps): Promise<string> => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log('1 f_One Register complete someProps:', someProps);
+      resolve('f_One_Register complete ');
+    }, 1000);
+  });
+};
+
+// we can make it wait for asyncf_Three_handleSubmit(
+///////// Start commenting here //////////
+const f_Two_login_Void = (someMoreProps): Promise<boolean> => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log('2 f_Two login complete with:', someMoreProps);
+      resolve('f_Two_login complete ');
+    }, 4000);
+  });
+};
+f_Three_handleSubmit(
+  f_One_Register('someProps').then(async (args) => {
+    await f_Two_login_Void('someMoreProps');
+  }),
+  'register'
+);
+///////// stop commenting here //////////
+
+// doesn't wait for async void
+///////// case of void async without await - Start commenting here //////////
+// const f_Two_login_Void = (someMoreProps): Promise<void> => {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       console.log('2 f_Two login complete with:', someMoreProps);
+//     }, 4000);
+//   });
+// };
+// f_Three_handleSubmit(
+//   f_One_Register('someProps').then((args) => {
+//     f_Two_login_Void('someMoreProps');
+//   }),
+//   'register'
+// );
+///////// stop commenting here //////////
